d5cce47f2e72df1bfaaa1c5bc07652f49b214117
fcogallegos <fgallegosgo@gmail.com>
2022-03-17 13:07:42 -0700

    validating that password and repeat password are equals

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,135 +5,156 @@ import Alert from '../components/Alert';
 
 const Register = () => {
 
-  const [ name, setName ] = useState('');
-  const [ email, setEmail ] = useState('');
-  const [ password, setPassword ] = useState('');
-  const [ repeatPassword, setRepeatPassword ] = useState('');
-  const [ alert, setAlert ] = useState({});
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [alert, setAlert] = useState({});
 
   const handleSubmit = e => {
-      e.preventDefault();
+    e.preventDefault();
 
-      if([ name, email, password, repeatPassword ].includes('')) {
-          setAlert({
-            msg: 'All the fields are required',
-            error: true
-          })  
-          return;
-      }
+    if ([name, email, password, repeatPassword].includes('')) {
+      setAlert({
+        msg: 'All the fields are required',
+        error: true
+      })
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      setAlert({
+        msg: 'The passwords are not equals',
+        error: true
+      })
+      return;
+    }
+
+    if (password.length < 6) {
+      setAlert({
+        msg: 'The password must be greater than 6 characters',
+        error: true
+      })
+      return;
+    }
+
+    setAlert({})
+
+    //create the user in the API
+    console.log('creating....');
   }
 
   const { msg } = alert;
 
   return (
-  <>
+    <>
 
-    <h1 
-      className="text-sky-600 font-black text-center text-5xl"
-    >Create your account
-    </h1>
+      <h1
+        className="text-sky-600 font-black text-center text-5xl"
+      >Create your account
+      </h1>
 
-    { msg && <Alert alert={alert} /> }
+      {msg && <Alert alert={alert} />}
 
-    <form className="my-5 bg-white shadow rounded-lg p-10"
-          onSubmit={handleSubmit}
-    >
-      <div className="my-5">
-          
-          <label 
-              className="text-gray-600 block text-xl font-bold"
-              htmlFor="name"
+      <form className="my-5 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
+        <div className="my-5">
+
+          <label
+            className="text-gray-600 block text-xl font-bold"
+            htmlFor="name"
           >Name</label>
 
-          <input 
+          <input
             id="name"
             type="text"
             placeholder="Introduce your Name"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             value={name}
-            onChange={ e => setName( e.target.value ) }
+            onChange={e => setName(e.target.value)}
           />
-      
-      </div>
 
-      <div className="my-5">
-          
-          <label 
-              className="text-gray-600 block text-xl font-bold"
-              htmlFor="email"
+        </div>
+
+        <div className="my-5">
+
+          <label
+            className="text-gray-600 block text-xl font-bold"
+            htmlFor="email"
           >Email</label>
 
-          <input 
+          <input
             id="email"
             type="email"
             placeholder="Register Email"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             value={email}
-            onChange={ e => setEmail( e.target.value ) }
+            onChange={e => setEmail(e.target.value)}
           />
-      
-      </div>
 
-      <div className="my-5">
-          
-          <label 
-              className="text-gray-600 block text-xl font-bold"
-              htmlFor="password"
+        </div>
+
+        <div className="my-5">
+
+          <label
+            className="text-gray-600 block text-xl font-bold"
+            htmlFor="password"
           >Password</label>
 
-          <input 
+          <input
             id="password"
             type="password"
             placeholder="Register Password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             value={password}
-            onChange={ e => setPassword( e.target.value ) }
+            onChange={e => setPassword(e.target.value)}
           />
-      
-      </div>
 
-      <div className="my-5">
-          
-          <label 
-              className="text-gray-600 block text-xl font-bold"
-              htmlFor="password2"
+        </div>
+
+        <div className="my-5">
+
+          <label
+            className="text-gray-600 block text-xl font-bold"
+            htmlFor="password2"
           >Repeat Password</label>
 
-          <input 
+          <input
             id="password2"
             type="password"
             placeholder="Repeat your Password"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
             value={repeatPassword}
-            onChange={ e => setRepeatPassword( e.target.value ) }
+            onChange={e => setRepeatPassword(e.target.value)}
           />
-      
-      </div>
 
-      <input 
-            type="submit"
-            value="Create Account"
-            className="bg-sky-700 mb-5 w-full py-3 
+        </div>
+
+        <input
+          type="submit"
+          value="Create Account"
+          className="bg-sky-700 mb-5 w-full py-3 
                 text-white font-bold rounded
                 hover:cursor-pointer hover:bg-sky-800 transition-colors"
 
-      />
+        />
 
-    </form>
+      </form>
 
-    <nav className="lg:flex lg:justify-between">
-          <Link
-              className="block text-center my-5 text-slate-500 uppercase text-sm"
-              to="/"
-          >Have an account yet? <br/> <span className="font-bold">Sign In.</span></Link>
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >Have an account yet? <br /> <span className="font-bold">Sign In.</span></Link>
 
-          <Link
-              className="block text-center font-bold my-5 text-slate-500 uppercase text-sm"
-              to="/forgot-password"
-          >Forgot my password.</Link>
-    </nav>
+        <Link
+          className="block text-center font-bold my-5 text-slate-500 uppercase text-sm"
+          to="/forgot-password"
+        >Forgot my password.</Link>
+      </nav>
 
-  </>
+    </>
   )
 }
 
