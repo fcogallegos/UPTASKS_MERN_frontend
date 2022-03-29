77fff08d314194c28fb2ab2a0790971e152dc272
fcogallegos <fgallegosgo@gmail.com>
2022-03-29 09:22:06 -0700

    creating the authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,24 @@ import ForgetPassword from './pages/ForgotPassword';
 import NewPassword from './pages/NewPassword';
 import ConfirmAccount from './pages/ConfirmAccount';
 
+import AuthProvider from './context/AuthProvider';
 
 function App() {
-  
+
 
   return (
     <BrowserRouter>
-      <Routes>
+      <AuthProvider>
+        <Routes>
           <Route path='/' element={<AuthLayout />} >
-              <Route index element={<Login />} />
-              <Route path='register' element={<Register />} />
-              <Route path='forget-password' element={<ForgetPassword />} />
-              <Route path='forget-password/:token' element={<NewPassword />} />
-              <Route path='confirm/:id' element={<ConfirmAccount />} />
+            <Route index element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='forget-password' element={<ForgetPassword />} />
+            <Route path='forget-password/:token' element={<NewPassword />} />
+            <Route path='confirm/:id' element={<ConfirmAccount />} />
           </Route>
-      </Routes>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
