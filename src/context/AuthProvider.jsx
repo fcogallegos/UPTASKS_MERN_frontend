8b83f8777f6818e5e790ad1c35a350c6dbfa292c
fcogallegos <fgallegosgo@gmail.com>
2022-03-30 11:49:36 -0700

    review the token for authenticate to user

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,11 +1,37 @@
 import { useState, useEffect, createContext } from 'react';
-
+import clientAxios from '../config/clientAxios';
 
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
 
     const [ auth, setAuth ] = useState({});
+
+    useEffect(() => {
+
+        const authenticateUser = async () => {
+            const token = localStorage.getItem('token');
+            if(!token) {
+                return;
+            }
+
+            const config = {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`
+                }
+            }
+
+            try {
+                const {data} = await clientAxios('/users/profile', config)
+                
+                setAuth(data);
+            } catch (error) {
+                
+            }
+        }
+        authenticateUser();
+    }, []);
 
     return (
         <AuthContext.Provider
