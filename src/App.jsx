7f877470ee93d33acd82daceb137958a8be6af80
fcogallegos <fgallegosgo@gmail.com>
2022-03-31 09:13:34 -0700

    check if the user is authenticated or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import AuthLayout from './layouts/AuthLayout';
+import ProtectedRoute from './layouts/ProtectedRoute';
+
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgetPassword from './pages/ForgotPassword';
 import NewPassword from './pages/NewPassword';
 import ConfirmAccount from './pages/ConfirmAccount';
+import Projects from './pages/Projects';
 
 import { AuthProvider }  from './context/AuthProvider';
 
@@ -21,6 +24,10 @@ function App() {
             <Route path='forget-password' element={<ForgetPassword />} />
             <Route path='forget-password/:token' element={<NewPassword />} />
             <Route path='confirm/:id' element={<ConfirmAccount />} />
+          </Route>
+
+          <Route path='/projects' element={<ProtectedRoute />}>
+            <Route index element={<Projects />} />
           </Route>
         </Routes>
       </AuthProvider>
---
+++ src/layouts/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { Outlet, Navigate } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
+const ProtectedRoute = () => {
+
+    const { auth } = useAuth();
+
+    console.log(auth);
+
+    return (
+        <>
+            { auth._id ? 'Authenticated' : <Navigate to="/" /> }
+        </>
+    )
+}
+
+export default ProtectedRoute
